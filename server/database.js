7db27eeadfb0213005b685c329bd76e23e2929f9
yeilop44 <pc_ylopez@hotmail.com>
2018-09-05 21:40:26 -0500

    commit

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-const URI = 'mongodb://localhost/MeanSearchFlights';
+const URI = 'mongodb://yeilop44:Fragante44@ds245772.mlab.com:45772/meansearchflights';
  
 mongoose.connect(URI)
 	.then(db => console.log('db is connected now'))
